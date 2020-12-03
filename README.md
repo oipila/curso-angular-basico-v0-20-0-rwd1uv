ec389e913201ec569f61702d0037c5a9e33d97e4
oipila <ipd.alipio@gmail.com>
2020-12-03 18:24:55 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# curso-angular-basico-v0-20-0-rwd1uv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/curso-angular-basico-v0-20-0-rwd1uv)
---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { InMemoryDataService } from './in-memory-data.service';
+import { MessagesComponent } from './messages/messages.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroAddComponent } from './hero-add/hero-add.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    NavbarComponent,
+    DashboardComponent,
+    HeroAddComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    NgbCollapseModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      delay: 500,
+    }),
+  ],
+  providers: [InMemoryDataService],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
